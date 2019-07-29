305bf43ff77fd34e668a0761288038327f215070
programgirl <michelle.a.gosse@gmail.com>
2019-07-30 11:30:33 +1200

    Removed toy Hello World example. Updated working Same sex couples function. Cannot get devtools::document() to work. I am getting the following console output:
    Updating PopSim documentation
    First time using roxygen2. Upgrading automatically...
    Loading PopSim
    Warning: Couples.R:19: Missing name
    Warning: The existing 'NAMESPACE' file was not generated by roxygen2, and will not be overwritten.

--- R/Couples.R
+++ R/Couples.R
@@ -1,16 +1,32 @@
+#' Create a subset of observations containing only same-sex couples
+#'
+#' This is a wrapper for randomly sampling observations into same-sex couples.
+#' It is mainly used for data frames that contain a subset of observations that require over-sampling.
+#' However, it can also be used to generate a simple random sample.
+#'
+#' @param x A data frame containing observations limited to one sex and includes an age column.
+#' @param ProbSameSex The probability of any observation being assigned to a same-sex couple.
+#' @param UpWeight If TRUE, a subset of ages will be over-sampled.
+#' @param UpWeightLowerAge The youngest age for the over-sampling. Required if UpWeight is TRUE.
+#' @param UpWeightUpperAge The oldest age for the over-sampling. Required if UpWeight is TRUE.
+#' @param AgeVariableIndex The column number of the data frame that contains the ages. Only used if over-sampling is specified. Required if UpWeight is TRUE.
+#'
+#' @return A data frame of an even number of observations for allocation into same-sex couples.
+# '
+
+# TODO examples.
+
 library("dplyr")
 #  bring in data - this uses the test file
 # HH3P <- readRDS("~/Sync/PhD/PopSim/R/HH3P.Rds")
-# split out the males for testing
 
+# split out the males for testing
 # HH3PPartneredMales <- HH3P %>%
 #   filter(SEX=="Male", RELATIONSHIP=="Partnered")
 
 
 # same sex couples
-Create.SameSexCouples <- function(dataframe, ProbSameSex = NULL, UpWeight = FALSE, ProbExpected = NULL, UpweightLowerAge = NULL, UpweightUpperAge = NULL) {
-
-  # TODO need age column number, test is age is integer
+Create.SameSexCouples <- function(x, ProbSameSex = NULL, UpWeight = FALSE, UpWeightProp = NULL, UpWeightLowerAge = NULL, UpWeightUpperAge = NULL, AgeVariableIndex = NULL) {
 
   # ProbExpected only used if UpWeight is not NULL, is the probability associated with the upweighted age range
   # UpWeightLowerAge/UpweightUpperAge only used if UpWeight is not NULL
@@ -25,64 +41,73 @@ Create.SameSexCouples <- function(dataframe, ProbSameSex = NULL, UpWeight = FALS
   }
 
   # create simple random sample without weights
-  if (is.null(UpWeight)) {
+  if (isFALSE(UpWeight)) {
     SameSexCouples <- dataframe[sample(1:CountPartneredCouples, NumberRequired, replace=FALSE),]
-    return(SameSexCouples)
-  }
+    }
 
   # create weighted samples
-  if (UpWeight == TRUE) {
-    if (is.null(ProbExpected) == TRUE) {
-      stop("An expected probability for upweighted ages must be provided.")
+  if (isTRUE(UpWeight)) {
+    if (is.null(UpWeightProp) == TRUE) {
+      stop("An expected proportion of upweighted ages must be provided.")
     }
-    if(is.null(UpweightLowerAge) == TRUE) {
-      stop("A minimum age for the upweights is required")
+    if(is.null(UpWeightLowerAge) == TRUE) {
+      stop("A minimum age for the upweights is required.")
     }
-    if(is.null(UpweightUpperAge) == TRUE) {
-      stop("A maximum age for the upweights is required")
+    if(is.null(UpWeightUpperAge) == TRUE) {
+      stop("A maximum age for the upweights is required.")
     }
+    if(is.null(AgeVariableIndex) == TRUE) {
+      stop("The column index for the age variable is required.")
+    }
+
+  # }
+
+  # get proportion of ages for upweighted observations
+
+  PropToUpWeight <- dataframe %>%
+    filter(dataframe[,8] >= UpWeightLowerAge & dataframe[,8] <= UpWeightUpperAge) %>%
+    summarise(Value=n()) %>%
+    mutate(PropResult = Value/nrow(dataframe)) %>%
+    select(PropResult) %>%
+    as.numeric()
+
+  UpWeightObs <- dataframe %>%
+    filter(dataframe[,8] >= UpWeightLowerAge & dataframe[,8] <= UpWeightUpperAge)
+
+  # check against actual proportion
+  # only adjust proportion if this differs to expected
+
+  if (PropToUpWeight != UpWeightProp) {
+
+    # upweight fix
+    # create upweighted subset
+    # create downweighted subset
+    # merge to form output dataset
+
+    UpWeightCount <- plyr::round_any(as.numeric((ProbSameSex*(UpWeightProp/PropToUpWeight)*nrow(UpWeightObs))), 2)
+
+    UpWeightObsSample <- UpWeightObs[sample(1:as.numeric(nrow(UpWeightObs)), UpWeightCount, replace=FALSE),]
+
+    DownWeightObs <- dataframe %>%
+      filter(dataframe[,8] < UpWeightLowerAge | dataframe[,8] > UpWeightUpperAge)
+
+    DownWeightObsSample <- UpWeightObs[sample(1:as.numeric(nrow(DownWeightObs)), (NumberRequired - UpWeightCount), replace=FALSE),]
+
+    SameSexCouples <- rbind(UpWeightObsSample, DownWeightObsSample)
+
+  } else {
+
+    # the expected and actual proportions are the same so just output a random sample
+
+    SameSexCouples <- dataframe[sample(1:CountPartneredCouples, NumberRequired, replace=FALSE),]
 
   }
 
-  #split file into high
-  #
-  # # higher probability is for age bands 6 through 8
-  # # get proportion of men in these age bands
-  # CountHH2PMaleSameSexBands <- Partnered2PHH %>%
-  #   filter(SEX=="Male" & AssignedAge >=25 &AssignedAge <=54) %>%
-  #   summarise(Value=n())
-  #
-  # PropHH2PMaleSameSexBands <- CountHH2PMaleSameSexBands/CountHH2PartneredMen
-  # # only 24% of partnered men in the higher same sex proportion age bands
-  # # but expectation is 67% so need to weight the age groups
-  # # need to upweight the higher-contributing ages
-  # # and make this an even number
-  # SameSexMaleHigher <- plyr::round_any(as.numeric((ProbSameSexManP2HH*(SameSexPeakAgeProp/PropHH2PMaleSameSexBands)*CountHH2PMaleSameSexBands)), 2)
-  #
-  # # select the count created from the loop above, from the high probability age bands
-  # Partnered2PHHSameSexHPDataframe <- Partnered2PHH %>%
-  #   filter(SEX=="Male", AssignedAge >=25, AssignedAge<=54)
-  # set.seed(161018)
-  # Partnered2PHHSameSexHP <- sample_n(Partnered2PHHSameSexHPDataframe,SameSexMaleHigher)
-  #
-  #
-  # # repeat for the lower-contributing ages
-  # SameSexMaleLower <- plyr::round_any(as.numeric((ProbSameSexManP2HH*((1-SameSexPeakAgeProp)/PropHH2PMaleSameSexBands)*CountHH2PMaleSameSexBands)), 2)
-  #
-  # Partnered2PHHSameSexLPDataframe <- Partnered2PHH %>%
-  #   filter(SEX=="Male", AssignedAge < 25 | AssignedAge > 54)
-  # set.seed(161018)
-  # Partnered2PHHSameSexLP <- sample_n(Partnered2PHHSameSexLPDataframe,SameSexMaleLower)
-  #
-  # # merge the two data frames into actual same sex data frame
-  # # both data frames extracted are even so no need to adjust at this point
-  # Partnered2PHHSameSexMen <- rbind(Partnered2PHHSameSexHP,Partnered2PHHSameSexLP)
-  # table(Partnered2PHHSameSexMen$AssignedAge)
-
-  # return(SameSexCouples)
+  }
+ return(SameSexCouples)
 }
 
-TestOpposite <- Create.SameSexCouples(HH3PPartneredMales, .1)
+# TestNoWeights <- Create.SameSexCouples(HH3PPartneredMales, .1)
 
 # values for testing below
 # dataframe <- HH3PPartneredMales
@@ -92,16 +117,18 @@ TestOpposite <- Create.SameSexCouples(HH3PPartneredMales, .1)
 # NumberRequired <- plyr::round_any(as.numeric(.1*CountPartneredCouples), 2)
 # SameSexCouples <- HH3PPartneredMales[sample(1:HH3PPartneredMales, NumberRequired, replace=FALSE),]
 
-
-# # opposite sex couples
-# Create.OppositeSexCouples <- function(dataframe, ) {
-#
-#   # TODO dataframe, lower age for upscaling, upper age for upscaling (test if either or both present)
-#   # TODO age column, expected prop same sex, expected prop upscaled age
-#
-#   # need to return a data frame/vector
-# }
+# testing for UpWeight == TRUE
 
 
-# saveRDS(HH3PPartneredMales, "HH3PPartneredMales.Rds")
-# saveRDS(HH3P, "HH3P.Rds")
+# AgeVariableIndex <- 8
+# #AgeVariable <- dataframe[,AgeVariableIndex]
+# UpWeight <- TRUE
+# ProbSameSex <- .1
+# UpWeightProp <- .6
+# UpWeightLowerAge <- 25
+# UpWeightUpperAge <- 54
+
+# put the above into an input format
+# TestWeights <- Create.SameSexCouples(HH3PPartneredMales , .1, TRUE, .6, 25, 54, 8)
+
+
