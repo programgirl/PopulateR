1cc1bf2cbaf2a7e84230c9c075164ab99ec438ef
Michelle <michelle.a.gosse@gmail.com>
2020-06-23 09:06:09 +1200

    Delete LoadingPhDDataFiles.R

--- R/LoadingPhDDataFiles.R
+++
@@ -1,6 +0,0 @@
-# Getting files into the project for testing
-HH4P <- readRDS("~/Sync/PhD/PopSim/R/HH4P.Rds")
-
-# bring in data frames from synethetic population
-File7FemaleCouple <- readRDS("~/Sync/PhD/PopSim/R/File7FemaleCouple.rds")
-File7MaleCouple <- readRDS("~/Sync/PhD/PopSim/R/File7MaleCouple.rds")
