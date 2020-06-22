ca75dfbf7dd0a4aba6a59f0b424446dd1187c9df
programgirl <michelle.a.gosse@gmail.com>
2020-06-23 09:28:01 +1200

    F you .gitignore

--- R/LoadingPhDDataFiles.R
+++ R/LoadingPhDDataFiles.R
@@ -4,3 +4,8 @@ HH4P <- readRDS("~/Sync/PhD/PopSim/R/HH4P.Rds")
 # bring in data frames from synethetic population
 File7FemaleCouple <- readRDS("~/Sync/PhD/PopSim/R/File7FemaleCouple.rds")
 File7MaleCouple <- readRDS("~/Sync/PhD/PopSim/R/File7MaleCouple.rds")
+
+# also using diffr to check files
+# use example diffr("~/Sync/PhD/PopSim/R/OppSexN.R", "~/Sync/PhD/PopSim/R/OppositeSexNewN.R")
+
+diffr("~/Sync/PhD/PopSim/R/OppSexSN.R", "~/Sync/PhD/PopSim/R/OppSexN.R")
