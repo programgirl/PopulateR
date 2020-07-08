1ea8c9dd452fa7c51c38edb6f7320944ff5f6ae1
programgirl <michelle.a.gosse@gmail.com>
2020-07-08 15:51:39 +1200

    still not working, getting wrong index. Cannot see reason as this isn't rows in sequence. Is always associated with an age difference of 26, but not all age differences of 26 have the problem.

--- R/AddChildLnLoop.R
+++ R/AddChildLnLoop.R
@@ -125,21 +125,19 @@ AddChildLnLoop <- function(Children, ChildIDVariable, ChildAgeVariable, Parents,
 #  cat("minimum age is ", MinParentAge, " and maximum age is ", MaxParentAge)
 
   for (j in 1:nrow(Children)) {
-#    Children$AgeDifference[j] <- round(rlnorm(1, meanlog=meanlogUsed, sdlog=sdlogUsed))
 
-   if (!(is.null(MinParentAge)) & !(is.null(MaxParentAge))) {
-
-     AgeDifference <- round(rlnorm(1, meanlog=meanlogUsed, sdlog=sdlogUsed))
+       if (!(is.null(MinParentAge)) & !(is.null(MaxParentAge))) {
+         AgeDifference <- round(rlnorm(1, meanlog=meanlogUsed, sdlog=sdlogUsed))
 
      if (isTRUE(AgeDifference >= MinParentAge & AgeDifference <=  MaxParentAge)) {
         MatchedAge <- Children[[ChildAgeVariable]][j] + AgeDifference
         age_index <- MatchedAge-(minIndexAge -1)
 
-        if (isTRUE(ParentAgeCountVector[age_index] != 0)) {
+        if (isTRUE(ParentAgeCountVector[age_index] > 0)) {
           Children$AgeDifference[j] <- AgeDifference
           Children$MatchedAge[j] <- MatchedAge
-           # Children$AgeIndex[j] <- age_index
-           ParentAgeCountVector[age_index] = ParentAgeCountVector[age_index] - 1
+          Children$AgeIndex[j] <- age_index
+          ParentAgeCountVector[age_index] = ParentAgeCountVector[age_index] - 1
 
 
          } else {
