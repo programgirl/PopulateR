063e9184cdbecf1a376211749639d656cd3cfb49
programgirl <michelle.a.gosse@gmail.com>
2020-11-02 10:08:18 +1300

    Function to re-allocate hours worked so that adolescents in school work fewer hours than adolescents who have left school. Leveraging off the school code. However, this code will take factors as well as numeric values. Parameters and their defintions and default values have been implemented. Still need to finish the preamble. Working on the preamble now.

---
+++ R/FixHours.R
@@ -0,0 +1,240 @@
+#' Reallocates the working hours of adolescents based on schooling.
+#' This function reallocates working hours so that adolescents in school work fewer hours than adolescents still in school. As hours worked may be conditional on sex, as well as age, the re-allocation is performed separately for each sex.
+#'
+#'
+#'
+#' The Leavers and Pyramid data frames should be summary data frames of counts. They must also have the same geopraphical base. For example, if the Leavers data is for the Canterbury region in New Zealand, then the Pyramid data must also be at the Canterbury region level of aggregation. In this example, the maximum aggregation level for the Adolescents data is Canterbury region, but could be for any sub-region, such as Christchurch city. However, the function should not be used if the Adolescents data is more aggregate, for example at the South Island or total New Zealand levels of aggregation
+#' For each year, counts of school leavers by age and sex are required. For example, if the school leaving age is 16, then the 2020 rows could be: 16-year-old females, 16-year-old males, 17-year-old females, 17-year-old males.
+#' The variables specifying sex can be numeric, character, or factor. The sole requirement is that the same code is used in all three data frames. For example, if "F" and "M" are used in the Adolescents data frame to denote sex, then "F" and "M" are the codes required in the Leavers and Pyramid data frames. Any number of values can be used, so long as they are unique.
+#' @export
+#' @param Adolescents A data frame containing all adolescents who have working hours.
+#' @param AdolescentSxVariable The column number for the variable that contain the codes specifying females and males.
+#' @param AdolescentAgeVariable The column number for the variable that contains the ages of the adolescents. This must be integer format.
+#' @param AdolescentInSchool The column number containing the indicator of whether an adolescent is in school or has left school. Can be either an ordered factor or numeric. If this is a factor, factor level 1 must be in-school. If it is a numeric variable, the lowest number must be the in-school value.
+#' @param HoursWorked The column number containing the hours worked by each adolescent. Must be an ordered factor or numeric. The levels/values must be ascending for hours worked.
+#' @param HoursCutOff The maximum hours worked by adolescents in-school. Must be the relevant factor level/number from HoursWorked.
+#' @param AgeWeight If age should be taken into account, when allocating hours. The default is to incorporate age. If yes, the default, hours worked will be allocated starting with the youngest adolescents still in school. If no, the only condition for re-allocating hours will be whether the adolescent is in school.
+#' @param UserSeed The user-defined seed for reproducibility. If left blank the normal set.seed() function will be used.
+
+
+FixHours <- function(Adolescents, AdolescentSxVariable = NULL, AdolescentAgeVariable = NULL, AdolescentInSchool = NULL, HoursWorked = NULL, HoursCutOff = NULL, AgeWeight = "Y", UserSeed = NULL) {
+
+  options(dplyr.summarise.inform=F)
+
+  #####################################
+  # quick reasonableness checks
+  #####################################
+
+  if (is.null(AdolescentSxVariable)) {
+    stop("The column number containing the sex information in the Adolescents data frame must be supplied.")
+  }
+
+  if (is.null(AdolescentAgeVariable)) {
+    stop("The column number containing the age information in the Adolescents data frame must be supplied.")
+  }
+
+  if (is.null(AdolescentsYear)) {
+    stop("The year in which the adolescents data was collected.")
+  }
+
+  # if (is.null(LeavingAge)) {
+  #   stop("The minimum school leaving age must be supplied.")
+  # }
+  #
+  # if (is.null(LeaversSxVariable)) {
+  #   stop("The column number containing the sex information in the Leavers data frame must be supplied.")
+  # }
+  #
+  # if (is.null(LeaversAgeVariable)) {
+  #   stop("The column number containing the age information in the Leavers data frame must be supplied.")
+  # }
+  #
+  # if (is.null(LeaversCount)) {
+  #   stop("The column number for the sex/age school leaver counts must be supplied.")
+  # }
+  #
+  # if (is.null(LeaversYear)) {
+  #   stop("The column number containing the year information in the LeaversData data frame must be supplied.")
+  # }
+  #
+  # if (is.null(PyramidSxVariable)) {
+  #   stop("The column number containing the sex information in the Pyramid data frame must be supplied.")
+  # }
+  #
+  # if (is.null(PyramidAgeVariable)) {
+  #   stop("The column number containing the age information in the Pyramid data frame must be supplied.")
+  # }
+  #
+  # if (is.null(PyramidCount)) {
+  #   stop("The column number for the sex/age information in the Pyramid data counts must be supplied.")
+  # }
+  #
+  # #####################################
+  # #####################################
+  # # rename variables so don't need to use quosures inside code
+  # #####################################
+  # #####################################
+  #
+  # # dataset names
+  # # cat("The children data frame is called", deparse(substitute(Adolescents)), "\n")
+  #
+  # Children <- as.data.frame(Adolescents %>%
+  #                             rename(Sex = !! AdolescentSxVariable, Age = !! AdolescentAgeVariable) %>%
+  #                             mutate(Sex = as.character(Sex),
+  #                                    Age = as.integer(Age)))
+  #
+  # Schooling <- as.data.frame(Leavers %>%
+  #                              rename(Sex = !! LeaversSxVariable, Age = !! LeaversAgeVariable, Year = !! LeaversYear, NumLeftSchool = !! LeaversCount) %>%
+  #                              mutate(Sex = as.character(Sex),
+  #                                     Age = as.integer(Age),
+  #                                     Year = as.integer(Year),
+  #                                     NumLeftSchool = as.integer(NumLeftSchool)) %>%
+  #                              select(Sex, Age, Year, NumLeftSchool))
+  #
+  #
+  # AgePyramid <- as.data.frame(Pyramid %>%
+  #                               rename(Sex = !! PyramidSxVariable, Age = !! PyramidAgeVariable, PyramidCount = !! LeaversCount) %>%
+  #                               mutate(Sex = as.character(Sex),
+  #                                      Age = as.integer(Age)) %>%
+  #                               select(Sex, Age, PyramidCount))
+  #
+  # # get the original variable names
+  #
+  # ChildrenAgeColName <- sym(names(Adolescents[AdolescentAgeVariable]))
+  # ChildrenSexColName <- sym(names(Adolescents[AdolescentSxVariable]))
+  #
+  # #####################################
+  # #####################################
+  # # end column names
+  # #####################################
+  # #####################################
+  #
+  # #####################################
+  # # check alignment of the three sex variables codes
+  # #####################################
+  #
+  # ChildrenSexCodes <- Children %>%
+  #   select(Sex) %>%
+  #   distinct(Sex) %>%
+  #   arrange(Sex)
+  #
+  # SchoolingSexCodes <- Schooling %>%
+  #   select(Sex) %>%
+  #   distinct(Sex) %>%
+  #   arrange(Sex)
+  #
+  # PyramidSexCodes <- AgePyramid %>%
+  #   select(Sex) %>%
+  #   distinct(Sex) %>%
+  #   arrange(Sex)
+  #
+  #
+  # if (isFALSE(identical(ChildrenSexCodes, PyramidSexCodes))) {
+  #
+  #   stop("The sex variable values are not the same for the ", deparse(substitute(Adolescents)), " and ", deparse(substitute(Pyramid)), " data frames.", "\n")
+  #
+  # }
+  #
+  # if (isFALSE(identical(ChildrenSexCodes, SchoolingSexCodes))) {
+  #
+  #   stop("The sex variable values for ", deparse(substitute(Leavers)), " differ from the other two data frames.", "\n")
+  #
+  # }
+  #
+  # #####################################
+  # #####################################
+  # # check leaver counts are summarised
+  # #####################################
+  # #####################################
+  #
+  # DuplicateTesting <- Schooling %>%
+  #   group_by(Year, Sex, Age) %>%
+  #   summarise(Duplicates = n()) %>%
+  #   filter(Duplicates > 1)
+  #
+  #
+  # if (!(is.na(DuplicateTesting$Year[1])) == TRUE) {
+  #
+  #   stop(deparse(substitute(Leavers)), " contains duplicates.", "\n")
+  # }
+  #
+  # #####################################
+  # #####################################
+  # # sum the leaver counts by current age
+  # #####################################
+  # #####################################
+  #
+  # Schooling <- Schooling %>%
+  #   filter(Year <= AdolescentsYear) %>%
+  #   mutate(Deduction = AdolescentsYear - Year,
+  #          CurrentAge = Age + Deduction) %>%
+  #   group_by(Sex, CurrentAge) %>%
+  #   summarise(TotalLeaverCount = sum(NumLeftSchool))
+  #
+  # ####################################
+  # ####################################
+  # # join after the leavers are summarised
+  # ####################################
+  # ####################################
+  # CombinedData <- Schooling %>%
+  #   left_join(AgePyramid, by = c("Sex", "CurrentAge" =  "Age")) %>%
+  #   mutate(PropLeft = TotalLeaverCount / PyramidCount,
+  #          PropLeft = ifelse(PropLeft > 1, 1, PropLeft)) %>%
+  #   filter(!(is.na(PropLeft))) %>%
+  #   rename(Age = CurrentAge) %>%
+  #   select(-(c(TotalLeaverCount, PyramidCount)))
+  #
+  #
+  # # remove any NAs as these will cause problems with the maths
+  # CombinedData <- CombinedData %>%
+  #   filter(!(is.na(PyramidCount)))
+  #
+  # cat("The proportion of adolescents who have left school are shown in the table below, by sex and age.", "\n")
+  #
+  # print(CombinedData)
+  #
+  #
+  # # join the probabilities to the children and calculate the probability of leaving school
+  # Children <- Children %>%
+  #   left_join(CombinedData, by = c("Age", "Sex")) %>%
+  #   mutate(PropLeft = ifelse(is.na(PropLeft), 0, PropLeft),
+  #          Status = "None")
+  #
+  # # seed must come before first sample is cut
+  # if (!is.null(UserSeed)) {
+  #   set.seed(UserSeed)
+  # }
+  #
+  # # apply the probabilities to the children
+  #
+  # for (x in 1:nrow(Children)) {
+  #
+  #   RandomRoll <- runif(1, 0, 1)
+  #
+  #   #   cat("Random roll is ", RandomRoll, " for the line ", Children$PropLeft[x], "\n")
+  #
+  #   if (isTRUE(RandomRoll <= Children$PropLeft[x])) {
+  #
+  #     #   print("Entered loop")
+  #
+  #     Children$Status[x] <- "Left"
+  #
+  #   } else {
+  #     Children$Status[x] <- "Stayed"
+  #
+  #     # closes random assignment to school statust
+  #   }
+  #
+  #   # closes loop through children data frame
+  # }
+  #
+  # Children <- Children %>%
+  #   mutate(!!ChildrenAgeColName := Age,
+  #          !!ChildrenSexColName := Sex) %>%
+  #   select(-c(PropLeft, Age, Sex))
+  #
+
+  return(Children)
+
+  #closes function
+}
