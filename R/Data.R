026c8a68a778c8d6d8af58962d09966580f84360
programgirl <michelle.a.gosse@gmail.com>
2021-06-28 14:26:24 +1200

    The file containing the meta data for the data frames included in the package.

---
+++ R/Data.R
@@ -0,0 +1,17 @@
+#' 2018 census records for the Timaru District in New Zealand
+#'
+#' A dataset produced from aggregate 2018 census data in TABLECODE 8395 on the
+#' official Stats NZ table builder website \url{http://nzdotstat.stats.govt.nz/wbos/Index.aspx}.
+#' Stats NZ’s data are licensed by Stats NZ for reuse under the Creative Commons Attribution
+#' 4.0 International licence.
+#'
+#' @format A data frame with 46,293 rows and 6 variables
+#' \describe{
+#'     \item{Sex}{Sex of the person}
+#'     \item{Age.group}{Age group in five-year bands}
+#'     \item{Relationship}{Relationship status of the person}
+#'     \item{LowerAge}{The youngest age in the Age.group}
+#'     \item{UpperAge}{The oldest age in the Age.group}
+#'     \item{ID}{The unique identifier for each person}
+#' }
+#' "Relationships"
