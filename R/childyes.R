bd99b3a72799703a1dbbcbd7be90c301ebb263de
programgirl <michelle.a.gosse@gmail.com>
2021-07-01 09:50:44 +1200

    childno - preamble amended, shortened, fixed text for non-matched adult returns.
    childyes - copied the childno code, modified it so that it should use the parent household id for the kids.

--- R/childyes.R
+++ R/childyes.R
@@ -1,7 +1,7 @@
 #' Create child- parent/guardian pairs with an added household identifier
 #' This function creates a data frame of child-parent/guardian pairs, based on a distribution of age differences. TThe function will use either a skew normal or normal distribution, depending on whether a skew ("alphaused") parameter is provided. The default value for the skew is 0, and using the default will cause a normal distribution to be used.
 #' Two data frames are required: one for children and one for potential parents.
-#' The minimum and maximum ages of parents must be specified. This ensures that there are no parents who were too young (e.g. 11 years) or too old (e.g. 70 years) at the time the child was born. The presence of too young and too old parents is tested throughout this function. Thus, pre-cleaning the parents data frame is not required..
+#' The minimum and maximum ages of parents must be specified. This ensures that there are no parents who were too young (e.g. 11 years) or too old (e.g. 70 years) at the time the child was born. The presence of too young and too old parents is tested throughout this function. Thus, pre-cleaning the parents data frame is not required.
 #'
 #' @export
 #' @param children A data frame containing the children to be paired with a parent/guardian.
@@ -13,8 +13,7 @@
 #' @param directxi If a skew-normal distribution is used, this is the location value. If the default alphaused value of 0 is used, this defaults to the mean value for the normal distribution.
 #' @param directomega If a skew-normal distribution is used, this is the scale value. If the default alphaused value of 0 is used, this defaults to the standard deviation value for the normal distribution.
 #' @param alphaused The skew. If a normal distribution is to be used, this can be omitted as the default value is 0 (no skew).
-#' @param hhidstart The starting number for generating the household identifier value that identifies a couple. Must be numeric.
-#' @param hhidvar The column name for the household variable. This must be supplied in quotes.
+#' @param hhidcol The column number for the household identifier variable in the parent data frame
 #' @param UserSeed The user-defined seed for reproducibility. If left blank the normal set.seed() function will be used.
 
 #' @return A list of three  data frames. $Matched contains the data frame of child-parent matches. $Adults contains any unmatched observations from the parents data frame. $Children contains any unmatched observations from the children data frame. $Adults and/or $Children may be empty data frames.
@@ -23,8 +22,7 @@
 # library(dplyr)
 
 childno <- function(children, chlidcol, chlagecol, parents, paridcol, paragecol, directxi, directomega,
-                    alphaused=0, minparage = NULL, maxparage = NULL, hhidstart = NULL, hhidvar= NULL,
-                    UserSeed=NULL)
+                    alphaused=0, minparage = NULL, maxparage = NULL, hhidcol = NULL, UserSeed=NULL)
 
 {
 
@@ -41,7 +39,7 @@ childno <- function(children, chlidcol, chlagecol, parents, paridcol, paragecol,
   }
 
   if (!any(duplicated(parents[paridcol])) == FALSE) {
-    stop("The column number for the ID variable in the parent data frame must be supplied, and the ID must be unique to each parent.")
+    stop("The column number for the ID variable in the parents data frame must be supplied, and the ID must be unique to each parent.")
   }
 
   if (is.null(minparage)) {
@@ -52,8 +50,9 @@ childno <- function(children, chlidcol, chlagecol, parents, paridcol, paragecol,
     stop("The maximum parent age must be supplied.")
   }
 
-  if(ncol(children) != ncol(parents)) {
-    stop("The number of columns does not match between the children and parents' data frames.")
+
+  if(is.null(hhidcol)) {
+    stop("The column number for the household identifier in the parents data frame must be supplied.")
   }
 
   #####################################
@@ -71,6 +70,9 @@ childno <- function(children, chlidcol, chlagecol, parents, paridcol, paragecol,
   parentsIDColName <- sym(names(parents[paridcol]))
 
   parentsAgeColName <- sym(names(parents[paragecol]))
+
+  parentsIDColName <- sym(names(parents[hhidcol]))
+
 
 
   #####################################
@@ -354,13 +356,10 @@ childno <- function(children, chlidcol, chlagecol, parents, paridcol, paragecol,
 
 
   # separate child and parent in data frames
-  Maxhhidstart <- (nrow(FullMatchedDataFrame)-1) + hhidstart
-
-#  cat("Dyad value constructed", "\n")
 
   childrenFinal <- FullMatchedDataFrame %>%
     ungroup() %>%
-    dplyr::select(all_of(1:NumberColschildren)) %>%
+    dplyr::select(all_of(1:NumberColschildren), all_of(parentsIDColName)) %>%
     rename_all(list(~gsub("\\.x$", "", .))) %>%
     mutate({{hhidvar}} := seq(hhidstart, Maxhhidstart))
 
