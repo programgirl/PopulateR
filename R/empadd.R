48c4cc17750736c0c499e6e959abf322d70e517f
programgirl <michelle.a.gosse@gmail.com>
2021-07-27 22:52:34 +1200

    aide memoire for writing up the preamble.

--- R/empadd.R
+++ R/empadd.R
@@ -4,6 +4,8 @@
 #' unemployed people can be included in the input data frame
 #' unemployed people will have an employer set to 0.
 #' FIX BELOW
+#'
+#' NOTE THAT EVERY COLUMN IN THE EMPLOYERS DF IS ADDED TO THE WORKER DF.
 #' This function constructs individual employers from aggregate counts, such as number of employers per employer type. Employer type is often industry, such as "Sheep, Beef Cattle and Grain Farming". Within each employer type, the number of employers is extracted. The number of employees is then randomly assigned to each of those employers, using the total employee count for that industry. A randomisation method is used to ensure that the company counts can be quite dissimilar across the employers within a type. However, this is constructed by the ratio of employers to employees. If the counts are similar, in this case the number of employees will tend to be 1 for each employer.
 #' END OF TO FIX
 #'
