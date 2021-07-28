0aa5bdfde0222477d5a7d532c4d30d729fa97965
programgirl <michelle.a.gosse@gmail.com>
2021-07-28 13:02:55 +1200

    Preamble finished.

--- R/empadd.R
+++ R/empadd.R
@@ -1,14 +1,8 @@
 #' Create a data frame of people with employers added
 #'
-#' accepts aggregate or disaggregate employee data
-#' unemployed people can be included in the input data frame
-#' unemployed people will have an employer set to 0.
-#' FIX BELOW
-#'
-#' NOTE THAT EVERY COLUMN IN THE EMPLOYERS DF IS ADDED TO THE WORKER DF.
-#' This function constructs individual employers from aggregate counts, such as number of employers per employer type. Employer type is often industry, such as "Sheep, Beef Cattle and Grain Farming". Within each employer type, the number of employers is extracted. The number of employees is then randomly assigned to each of those employers, using the total employee count for that industry. A randomisation method is used to ensure that the company counts can be quite dissimilar across the employers within a type. However, this is constructed by the ratio of employers to employees. If the counts are similar, in this case the number of employees will tend to be 1 for each employer.
-#' END OF TO FIX
-#'
+#' This function creates a data frame of people and matching employers, if employed. Two data frames are required: one for the population and one for the employers.
+#' A numeric or ordered factor for working hours is required. The minimum value for being in employment must be specified. Anyone coded under this value will be treated as unemployed. Thus, pre-cleaning the population data frame is not required.
+#' The employer data frame can be either a summary in the form of the number of employees by employer. The other option is that each row represents a vacancy for an employee. Thus, an employer with 5 employees may be represented as either: a single row with an employee count of 5, or 5 rows with an employee count of 1 in each row.
 #' @export
 #' @param employers A data frame containing employer data.
 #' @param empid The column number for the employer ID.
@@ -20,15 +14,12 @@
 #' @param userseed The user-defined seed for reproducibility. If left blank the normal set.seed() function will be used.
 #'
 #' @return
-#'FIX BELOW
-#'
-#'    #'A list of three data frames $Companies contains the data frame of synthetic companies, with the number of employees and a mock company name. $Overcounts contains the companies where the number of employers in an industry exceeds the number of employees. This is an informational data frame provides the original values for employee and employee counts. These industries are included in the $Companies file. The count of employers is reset to the count of employees, resulting in synthetic companies with employees count of 1. $NoEmps contains industries with employer counts but no employee counts, employee counts but no employer counts, and a combination of no employers and no employees. These are excluded from the $Companies data frame. Industries with 0 employee and 0 employer counts is likely due to a standardised list of industries being used for all geographic regions. The existence of employers with no employees is indicative of sole-trader/director-only companies. The presence of employees but no employers suggests a data accuracy problem.
+#' A data frame of the population, with an employer ID attached to each person. Unemployed people will have an employer ID of 0. All columns in the employers data frame, except for the employee counts, are included in the output data frame.
 #'
 #' @example
-#' library("dplyr")
+#'library("dplyr")
 #'
-#' TownshipEmployment <- empcreate(AllEmployers, emptypecol = 1, empnumcol = 2, staffnumcol = 3, userseed = 4)
-#' END OF TO FIX
+#'EmployedPeople <- empadd(EmployerSet, empid = 3, empcount = 2, Township, wrkid = 3, hourscol = 5, hoursmin = 2, userseed = 4)
 
 empadd <- function(employers, empid, empcount, workers, wrkid, hourscol, hoursmin, userseed = NULL) {
 
