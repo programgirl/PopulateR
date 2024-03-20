a86f913abd7acd4458e9fd7b35ecacf994f42b45
programgirl <michelle.a.gosse@gmail.com>
2024-03-20 23:42:06 +1300

    childrenno replaced with function pairchild
    otherNum - amended the preamble

--- R/otherNum.R
+++ R/otherNum.R
@@ -1,16 +1,16 @@
 #' Create a match of people into existing households
 #' This function creates a data frame of household inhabitants, with the specified number of inhabitants.
-#' Two data frames are required. The 'existing' data frame contains the people already in households. The 'additions' data frame contains the people. The use of an age distribution for the matching ensures that an age structure is present in the households. A less correlated age structure can be produced by entering a larger standard deviation.
+#' Two data frames are required. The 'existing' data frame contains the people already in households. The 'additions' data frame contains the people who are not in households. The use of an age distribution for the matching ensures that an age structure is present in the households. A less correlated age structure can be produced by entering a larger standard deviation.
 #' The output data frame of matches will only contain households of the required size.
 #'
 #' @export
 #' @param existing A data frame containing the people already in households.
-#' @param exsid The column number for the ID variable, for people in the existing data frame.
-#' @param exsage The column number for the Age variable, for people in the existing data frame.
+#' @param exsid The ID variable for people in the existing data frame.
+#' @param exsage The age variable, for people in the existing data frame.
 #' @param HouseholdNumVariable The name of the household variable in the data frame containing the people already in households.
 #' @param additions A data frame containing the people to be added to the existing households.
-#' @param addid The column number for the ID variable, for people to be added to the existing households.
-#' @param addage The column number for the Age variable, for people in the existing data frame.
+#' @param addid The ID variable for people to be added to the existing households.
+#' @param addage The ag variable, for people in the existing data frame.
 #' @param numadd The number of people to be added to the household.
 #' @param sdused The standard deviation of the normal distribution for the distribution of ages in a household. If no standard deviation is specified, the function will output all initial matches.
 #' @param userseed The user-defined seed for reproducibility. If left blank the normal set.seed() function will be used.
@@ -36,54 +36,54 @@
 otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, additions, addid, addage,
                      numadd = NULL, sdused = NULL, userseed=NULL, attempts= 10, numiters = 10000)
 {
-  
+
   options(dplyr.summarise.inform=F)
-  
+
   # content check
   if (is.null(numadd)) {
     stop("The number of people to be added to the household must be supplied.")
   }
-  
-  
+
+
   #####################################
   #####################################
   # get column names as symbols to use inside data frame subfunctions
   #####################################
   #####################################
-  
+
   # smalldf ID variable
   exsidcolName <- sym(names(existing[exsid]))
   # smalldf age variable
   exsagecolName <- sym(names(existing[exsage]))
-  
+
   # largedf ID variable
   addidcolName <- sym(names(additions[addid]))
   # largedf age variable
   addagecolName <- sym(names(additions[addage]))
-  
-  
-  
+
+
+
   if (!any(duplicated(existing[HouseholdNumVariable])) == FALSE) {
     stop("The column number for the household ID variable in the 'existing' data frame must be supplied, and the household number must be unique to each person")
   }
-  
+
   if (!any(duplicated(existing[[exsid]])) == FALSE) {
     stop("The column number for the ID variable in the parent data frame must be supplied, and the ID must be unique to each person")
   }
-  
+
   if (!any(duplicated(additions[[addid]])) == FALSE) {
     stop("The column number for the ID variable in the 'additions' data frame must be supplied, and the ID must be unique to each person")
   }
 
-  
+
   #######################
   # set up user seed
-  
+
   if (!is.null(userseed)) {
     set.seed(userseed)
   }
-  
-  
+
+
   #####################################
   #####################################
   # sub functions are here
@@ -93,53 +93,53 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
 
   if(!is.null(sdused) == TRUE) {
   # pairing swap subfunction
-  
+
   swap_household_matches <- function(pair1, pair2) {
     swap <- pair1
     swap$MatchedID <- pair2$MatchedID
     swap$MatchedAge <- pair2$MatchedAge
     return(swap)
   }
-  
- 
+
+
    # set up bins and variables for matching
   # set up bins for iterations
   # enable at least some extreme age differences to be assigned to the Inf categories
   # otherwise the bins will be wrong
-  
+
   MaxAgeDifference <-  (max(existing[exsage]) -
                           min(existing[exsage]))-5
-  
+
   # estimate expected minimum and maximum ages from the distribution, and bin these
-  
+
   min_bin <- round(qnorm(0.000001, mean = 0, sd = sdused))-0.5
   max_bin <- round(qnorm(0.999999, mean = 0, sd = sdused))+0.5
   bins <- c(-Inf, min_bin:max_bin, Inf)
-  
+
   # construct the probabilities for each bin, gives n(bins)-1
   Probabilities <- pnorm(bins[-1], mean = 0, sd = sdused) -
     pnorm(bins[-length(bins)], mean = 0, sd = sdused)
-  
+
   # assign realistic expected probabilities in the bins outside the bins constructed earlier
   # use minAge and maxAge for this, only need range for included ages
   # Uses midpoint rule.
   logProbLow <- dnorm(-MaxAgeDifference:(min_bin-0.5), mean = 0, sd = sdused, log=TRUE)
   logProbHigh <- dnorm((max_bin+0.5):MaxAgeDifference, mean = 0, sd = sdused, log=TRUE)
-  
+
   logProb <- c(logProbLow, log(Probabilities[-c(1, length(Probabilities))]), logProbHigh)
   logBins    <- c(-Inf, -(MaxAgeDifference-.5):(MaxAgeDifference-.5), Inf)
-  
- 
-  
-  
+
+
+
+
    #####################################
   # chi-squared check subfunction
   #####################################
-  
+
   compare_logK <- function(prop, curr) {
     # what we want to do is know if sum(exp(prop)) > sum(exp(curr))
     # but we can't work out exp(prop) or exp(curr) during the process..
-    
+
     # to do this, we first eliminate those that don't matter
     w = prop != curr
     if (sum(w) == 0) {
@@ -147,7 +147,7 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
     }
     prop = prop[w]
     curr = curr[w]
-    
+
     # next we find which is the dominant exponent, as changes these are all that will matter
     # i.e. we write exp(a) + exp(b) = exp(a)[1 + exp(b-a)] where a > b, so that the additional terms are less than 1
     # and we can exponentiate them safely. We then ignore the base (it's common) and just use extras
@@ -156,150 +156,150 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
     curr = curr - base
     sum(exp(prop)) - sum(exp(curr))
   }
-  
+
   # closes if(!is.null(sdused) == TRUE)
-  } 
-  
+  }
+
   ####################################
   ####################################
   # sub functions end
   ####################################
   ####################################
-  
-  
-  
-  
-  
+
+
+
+
+
   #####################################
   #####################################
   # create internal data frames
   #####################################
   #####################################
-  
-  
+
+
   existingRenamed <- as.data.frame(existing %>%
-    rename(existID = !! exsidcolName, 
+    rename(existID = !! exsidcolName,
            existAge = !! exsagecolName,
            internalHHID = !! HouseholdNumVariable) %>%
     ungroup())
-  
-  
+
+
   additionsRenamed <- as.data.frame(additions %>%
-    rename(addID = !! addidcolName, 
+    rename(addID = !! addidcolName,
            addAge = !! addagecolName) %>%
     ungroup())
 
   existingOriginal <- existingRenamed
-  
+
   additionsOriginal <- additionsRenamed
 
 
 
 
-  
+
   #####################################
   #####################################
   # get column names as symbols to use inside data frame subfunctions
   #####################################
   #####################################
-  
- 
-  
-  
-  
+
+
+
+
+
    #####################################
   #####################################
-  
+
   # work out data frame compatibility
   # the additions data frame may be too small or too large based on the size of the
   # existing count and the number of people to add
-  
+
   # get the length of the existing data frame
-  
+
   NumExisting <- as.numeric(nrow(existingRenamed))
-  
+
   NumNeeded <- NumExisting * numadd
-  
+
   NumProvided <- as.numeric(nrow(additionsRenamed))
 
   if(NumNeeded > NumProvided) {
-    
+
     cat("The additions data frame should contain", NumNeeded, "people but only contains", NumProvided, "\n")
-    
+
     NumCanUse <- floor(NumProvided / numadd)
-    
+
     cat(NumCanUse, "will be randomly sampled from the", NumExisting, "people already in households \n")
-    
+
     existingRenamed <- existingRenamed %>%
       slice_sample(n = NumCanUse)
   }
-  
+
   if(NumNeeded < NumProvided) {
-    
+
     cat("The additions data frame should contain", NumNeeded, "people and contains", NumProvided, "\n")
-    
+
     cat(NumNeeded, "will be randomly sampled from the", NumProvided, "people to add to households \n")
-    
+
     additionsRenamed <- additionsRenamed %>%
       slice_sample(n = NumNeeded)
-    
+
   }
-  
+
 
   #####################################
   # matching
   #####################################
-  
-  
+
+
   BaseSize <- as.numeric((nrow(existingRenamed)))
 
 
   for (i in 1: numadd) {
-    
+
     # if(BaseSize < 1) {
     #   stop("Sample size is less than 1", "\n")
     # }
-    
+
     MatchingSample <- additionsRenamed %>%
       slice_sample(n = BaseSize, replace = FALSE)
 
        # cat("MatchingSample size is", nrow(MatchingSample), "\n")
-    # 
+    #
     additionsRenamed <- additionsRenamed %>%
       filter(!(addID %in% MatchingSample$addID))
-    
+
     # get age differences
-    
+
     CurrentAgeMatch <- existingRenamed %>%
-      select(internalHHID, existAge, existID) 
-    
+      select(internalHHID, existAge, existID)
+
     MatchedAgeExtract <- MatchingSample %>%
       select(addAge, addID)
-    
+
     CurrentAgeMatch <- cbind(CurrentAgeMatch, MatchedAgeExtract)
-    
+
     # cat("Current age match is", nrow(CurrentAgeMatch), "Matched age extract is",
     #     nrow(MatchedAgeExtract), "combined age match is", nrow(CurrentAgeMatch), "\n")
-    
+
     # ONLY ITERATE MATCHES IF THERE IS A SD
     if(!is.null(sdused) == TRUE) {
-    
+
     ExpectedAgeProbs <- Probabilities * nrow(CurrentAgeMatch)
     logEAgeProbs <- logProb + log(nrow(CurrentAgeMatch))
-    
+
     ObservedAgeDifferences <- hist(CurrentAgeMatch[,1] - CurrentAgeMatch[,3],
                                    breaks = bins, plot=FALSE)$counts
-    
-    
+
+
     # set up for chi-squared
     log0ObservedAges <- hist(CurrentAgeMatch[,1] - CurrentAgeMatch[,3],
                              breaks = logBins, plot=FALSE)$counts
     logKObservedAges = ifelse(log0ObservedAges == 0, 2*logEAgeProbs,
                               log((log0ObservedAges - exp(logEAgeProbs))^2)) - logEAgeProbs
     log_chisq = max(logKObservedAges) + log(sum(exp(logKObservedAges - max(logKObservedAges))))
-    
+
     # print(log_chisq)
-    
+
 
     Critical_log_chisq <- log(qchisq(0.01, df=(length(logEAgeProbs-1)), lower.tail = TRUE))
 
@@ -311,80 +311,80 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
     # iteration for matching  ages starts here
     #####################################
     #####################################
-    
+
     # str(CurrentAgeMatch)
-    
+
     # cat("Gets to matching age iterations", "\n")
-    
+
     if(log_chisq > Critical_log_chisq) {
-      
+
     for (j in 1:numiters) {
-      
+
       # print(j)
-      
-      
+
+
       # randomly choose two pairs
       Pick1 <- sample(nrow(CurrentAgeMatch), 1)
       Pick2 <- sample(nrow(CurrentAgeMatch), 1)
       Current1 <- CurrentAgeMatch[Pick1,]
       Current2 <- CurrentAgeMatch[Pick2,]
-      
+
       # # proposed pairing after a swap
       PropPair1 <- swap_household_matches(Current1, Current2)
       PropPair2 <- swap_household_matches(Current2, Current1)
-      
+
       # compute change in Chi-squared value from current pairing to proposed pairing
       PropAgeMatch <- CurrentAgeMatch %>%
         filter(!(existID %in% c(PropPair1[,2], PropPair2[,2]))) %>%
         bind_rows(., PropPair1,PropPair2)
-      
+
       # cat("PropAgeMatch has", nrow(PropAgeMatch), "rows", "\n")
-      
+
       # do chi-squared
       Proplog0 <- hist(PropAgeMatch[,1] - PropAgeMatch[,3], breaks = logBins, plot=FALSE)$counts
       ProplogK = ifelse(Proplog0 == 0, 2*logEAgeProbs,
                         log((Proplog0 - exp(logEAgeProbs))^2)) - logEAgeProbs
-      
+
       prop_log_chisq = max(ProplogK) + log(sum(exp(ProplogK - max(ProplogK))))
-      
+
       if (compare_logK(ProplogK, logKObservedAges) < 0) {
-        
+
         # cat("Loop entered", prop_log_chisq, "\n")
-        
+
         CurrentAgeMatch[Pick1,] <- PropPair1
         CurrentAgeMatch[Pick2,] <- PropPair2
-        
-        
+
+
         log0ObservedAges <- Proplog0
         logKObservedAges <- ProplogK
         log_chisq <- prop_log_chisq
-        
+
         cat("log chi-square is", log_chisq, "\n")
-        
+
         # closes pair swqp
-        
+
       }
 
-      
-      
+
+
       if (log_chisq <= Critical_log_chisq) {
-        
-        
+
+
         break
-        
+
         }
-      
+
       # closes for (j in 1:numiters) {
     }
-      
+
       cat(j, "iterations were used, and the final chi-squared value was", round(log_chisq,6), "\n")
-      
+
       # closes if(log_chisq > Critical_log_chisq) {
     }
-    
+
     # closes if(!is.null(sdused) == TRUE) {
     }
-    
+
     #####################################
     #####################################
     # getting the data frames ready for output
@@ -394,27 +394,27 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
     # only need to do full matching at the end
     #####################################
     #####################################
-    
+
 
     if(exists("theMatched")) {
-      
+
 
       theMatched <- bind_rows(theMatched, CurrentAgeMatch)
 
       # no need to delete
-      
+
     } else {
-      
-      theMatched <- CurrentAgeMatch 
-      
+
+      theMatched <- CurrentAgeMatch
+
      # closes else to if(exists("othersMatched")) {
     }
-    
+
     # closes for (i in 1: numadd) {
     # i.e. closes loop through each addition needed
 
   }
-  
+
   # the data frame theMatched is now a long one with a duplicate set of base ids if > 1 person needed matching
 
 
@@ -423,42 +423,42 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
   # create dataframes for output
   #####################################
   #####################################
-  
+
   HHIDcolName <- sym(names(existing[HouseholdNumVariable]))
-  
+
   # the data frame that already had a household ID
 
   baseMini <- theMatched %>%
     select(existID) %>%
     unique()
-  
+
 
   # merge in the extra info
   theOriginal <- left_join(baseMini, existingOriginal, by = c("existID"))
-  
+
   theOriginal <- theOriginal %>%
-    rename(!!exsidcolName := existID, 
+    rename(!!exsidcolName := existID,
            !!exsagecolName := existAge,
            !!HHIDcolName := internalHHID)
 
   # the data frame that contributed new people to the household
-  
+
   # data for the matched people
    addedMini <- theMatched %>%
      select(addID, internalHHID)
-   
-   
+
+
    # merge in their details
    theMatched <- left_join(addedMini, additionsOriginal, by = c("addID"))
 
    theMatched <- theMatched %>%
-     rename(!!addidcolName := addID, 
+     rename(!!addidcolName := addID,
             !!addagecolName := addAge,
             !!HHIDcolName := internalHHID)
-   
+
 
    # merge the two into the matched output data frame
-   
+
   OutputDataframe <- bind_rows(theOriginal, theMatched)
 
   cat("The individual dataframes are $Matched, $Existing, and $Additions", "\n")
@@ -470,14 +470,14 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
 
   UnmatchedExisting <- existing %>%
     filter(!({{exsidcolName}} %in% MatchedIDs))
-  
+
   # UnmatchedExisting was a tibble, return a data frame
   UnmatchedExisting <- as.data.frame(UnmatchedExisting)
 
   UnmatchedAdditions <- additions %>%
     filter(!({{addidcolName}} %in% MatchedIDs)) # this filter is not working
-  
-  
+
+
   MergedList <- list()
 
   MergedList$Matched <- OutputDataframe
@@ -485,6 +485,6 @@ otherNum <- function(existing, exsid, exsage, HouseholdNumVariable = NULL, addit
   MergedList$Additions <- UnmatchedAdditions
 
   return(MergedList)
-  
+
 }
 
