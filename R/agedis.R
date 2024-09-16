b037772aea83ed48dc073f58ff567ba232ea3fde
programgirl <michelle.a.gosse@gmail.com>
2024-09-17 11:57:10 +1200

    Fixed missing full stops in lines 3-4. No idea why line 13 keeps showing as changed.

--- R/agedis.R
+++ R/agedis.R
@@ -1,7 +1,7 @@
 #' Add a sex/age structure to a data frame of grouped ages.
 #' This function creates a data frame that adds an age structure to a data frame that contains age groups.
-#' Two data frames are required: the data frame that contains individuals with age bands ("individuals"), and a data frame used as the basis for constructing a sex/age pyramid ("pyramid"). 
-#' The individuals data frame requires two columns relating to the age groups. One is the minimum age in the age group The second is the maximum age in the age group For example, the age group 0 - 4 years would have 0 as the minimum age value and 4 as the maximum age value. Each person in the individuals data frame must have both the minimum and maximum age variables populated.
+#' Two data frames are required: the data frame that contains individuals with age bands ("individuals"), and a data frame used as the basis for constructing a sex/age pyramid ("pyramid").
+#' The individuals data frame requires two columns relating to the age groups. One is the minimum age in the age group. The second is the maximum age in the age group. For example, the age group 0 - 4 years would have 0 as the minimum age value and 4 as the maximum age value. Each person in the individuals data frame must have both the minimum and maximum age variables populated.
 #' The pyramid data frame must contain counts by sex/age in the population of interest.
 #' The variables specifying sex can be numeric, character, or factor. The sole requirement is that the codes must match.  For example, if "F" and "M" are used in the individuals data frame to denote sex, then "F" and "M" are the codes required in the pyramid data frame. Any number of sex code values can be used, so long as they are unique.
 #' @export
@@ -10,7 +10,7 @@
 #' @param minage The variable containing the minimum age for the age group, in the individuals data frame.
 #' @param maxage The variable containing the maximum age for the age group, in the individuals data frame.
 #' @param pyramid A data frame containing the sex/age pyramid to be used.
-#' @param pyrsx The variable containing the codes for sex, in the pyramid data frame. 
+#' @param pyrsx The variable containing the codes for sex, in the pyramid data frame.
 #' @param pyrage The variable containing the ages, in the pyramid data frame.
 #' @param pyrcount The variable containing the counts for each sex/age combination, in the pyramid data frame.
 #' @param agevarname The name to use for the constructed age variable in the output data frame. For each row, this will contain one integer.
@@ -32,32 +32,32 @@ agedis <- function(individuals, indsx, minage, maxage, pyramid, pyrsx, pyrage, p
   #####################################
   # check for missing input information
   #####################################
-  
+
   if (!indsx %in% names(individuals)) {
     stop("Sex variable name in the individuals data frame is incorrect.")
   }
-  
+
   if (!minage %in% names(individuals)) {
     stop("Minimum age variable name in the individuals data frame is incorrect.")
   }
-  
+
   if (!maxage %in% names(individuals)) {
     stop("Maximum age variable name in the individuals data frame is incorrect.")
   }
-  
+
   if (!pyrsx %in% names(pyramid)) {
     stop("Sex variable name in the pyramid data frame is incorrect.")
   }
-  
+
   if (!pyrage %in% names(pyramid)) {
     stop("Age variable name in the pyramid data frame is incorrect.")
   }
-  
+
   if (!pyrcount %in% names(pyramid)) {
     stop("Variable name for the age counts in the pyramid data frame is incorrect.")
   }
 
- 
+
   # #####################################
   # #####################################
   # # rename variables so don't need to use quosures inside code
@@ -71,7 +71,7 @@ agedis <- function(individuals, indsx, minage, maxage, pyramid, pyrsx, pyrage, p
   Agepyramid <- as.data.frame(pyramid %>%
     rename(Sex = !! pyrsx, Age = !! pyrage, Prob = !! pyrcount) %>%
     mutate(Sex = as.character(Sex)))
-  
+
   #####################################
   #####################################
   # end column names
@@ -98,7 +98,7 @@ agedis <- function(individuals, indsx, minage, maxage, pyramid, pyrsx, pyrage, p
     stop("The sex variable values are not the same for both data frames.")
 
   }
-  
+
 
   #####################################
   #####################################
@@ -116,21 +116,21 @@ agedis <- function(individuals, indsx, minage, maxage, pyramid, pyrsx, pyrage, p
   # for (x in 1:10) {
 
     CurrentIndividual <- BaseDataFrame[x,]
-    
+
  #   print("Selected current individual")
 
     AgeRestricted <- Agepyramid %>%
       filter(between(Age, CurrentIndividual$MinAge, CurrentIndividual$MaxAge),
              Sex == CurrentIndividual$Sex) %>%
       select(Age, Prob)
-    
+
  #  print("Constructed age pyramid of correct ages")
 
     OutputAge <- AgeRestricted %>%
       slice_sample(n=1, weight_by = Prob) %>%
       select(Age) %>%
       pull(Age)
-    
+
 #    print("Probabilistic age selected")
 
     CurrentIndividual <- CurrentIndividual %>%
