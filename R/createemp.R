71f3f18f00eefbd269ed00c9627dbae032a8e9b0
programgirl <michelle.a.gosse@gmail.com>
2024-09-17 13:22:54 +1200

    Preamble updated. Starting fixing function call example, but stopped as need to prep file.

--- R/createemp.R
+++ R/createemp.R
@@ -1,6 +1,6 @@
 #' Create a data frame of individual employers, each with aggregate employee counts.
 #'
-#' This function constructs individual employers from aggregate counts, such as number of employers per employer type. Employer type is often industry, such as "Sheep, Beef Cattle and Grain Farming". Within each employer type, the number of employers is extracted. The number of employees is then randomly assigned to each of those employers, using the total employee count for that industry. A randomisation method is used to ensure that the company counts can be quite dissimilar across the employers within a type. However, this is constructed by the ratio of employers to employees. If the counts are similar, in this case the number of employees will tend to be 1 for each employer.
+#' This function constructs individual employers from aggregate counts, such as number of employers per employer type. Employer type is often industry, such as "Sheep, Beef Cattle and Grain Farming". Within each employer type, the number of employers is extracted. The number of employees is then randomly assigned to each of those employers, using the total employee count for that industry. A randomisation method is used to ensure that the company counts can be quite dissimilar across the employers within a type. However, this is constructed by the ratio of employers to employees. If the number of employers is similar to the number of employees, the number of employees will tend to be 1 for each employer.
 #'
 #' @export
 #' @param employers A data frame containing aggregate data on employers.
@@ -11,27 +11,28 @@
 #' @param pplmax The variable containing the maximum number of staff in each industry.
 #' @param stffname The variable name to use for the staff counts for each employer.
 #' @param cpyname The variable name to use for the companies.
-#' @param userseed The user-defined seed for reproducibility. If left blank the normal set.seed() function will be used.
+#' @param userseed If specified, this will set the seed to the number provided. If not, the normal set.seed() function will be used.
 #'
-#' @return #'A list of three data frames $Companies contains the data frame of synthetic companies, with the number of employees and a mock company name. $Overcounts contains the companies where the number of employers in an industry exceeds the number of employees. This is an informational data frame provides the original values for employee and employee counts. These industries are included in the $Companies file. The count of employers is reset to the count of employees, resulting in synthetic companies with employees count of 1. $NoEmps contains industries with employer counts but no employee counts, employee counts but no employer counts, and a combination of no employers and no employees. These are excluded from the $Companies data frame. Industries with 0 employee and 0 employer counts is likely due to a standardised list of industries being used for all geographic regions. The existence of employers with no employees is indicative of sole-trader/director-only companies. The presence of employees but no employers suggests a data accuracy problem.
+#' @return #'A data frames of synthetic companies, with the number of employees and a mock company name.
 #'
 #' @examples
 #' library("dplyr")
 #'
-#' TownshipEmployment <- empcreate(AllEmployers, emptypecol = 1, empnumcol = 2,
+#' TownshipEmployment <- createemp(AllEmployers, industry = 1, indsmin = 2, indsmax = , pplmin = ,
+#'
 #'                                 staffnumcol = 3, userseed = 4)
 
 createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stffname=NULL, cpyname=NULL, userseed = NULL) {
-  
+
   options(dplyr.summarise.inform=F)
-  
+
   if(is.null(stffname)) {
-    
+
     stop("The name of the variable containing the staff counts must be supplied.")
   }
-  
+
   if(is.null(cpyname)) {
-    
+
     stop("The name of the variable containing the company names must be supplied.")
   }
 
@@ -58,7 +59,7 @@ createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stf
            companyCtMax = !! empmaxcolName,
            staffCtMin = !! pplmincolName,
            staffCtMax = !! pplmaxcolName)
-  
+
 
 
   # put in the starting value for the paste0 used in the company name
@@ -78,33 +79,33 @@ createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stf
 
   for (i in 1:nrow(employerRenamed)) {
 
-    currentEmp <- employerRenamed[i,] 
+    currentEmp <- employerRenamed[i,]
 
     minEmp <- currentEmp$companyCtMin
     maxEmp <- currentEmp$companyCtMax
 
     numEmp <- round(runif(1, minEmp, maxEmp),0)
-    
+
     minStaff <- currentEmp$staffCtMin
     maxStaff <- currentEmp$staffCtMax
-    
+
     numStaff <- round(runif(1, minStaff, maxStaff),0)
-    
+
     # cat("minEmp is", minEmp, "maxEmp is", maxEmp, "minStaff is", minStaff, "maxStaff is", maxStaff, "numEmp is", numEmp, "numStaff is", numStaff, "\n")
-    
+
     if(numStaff < numEmp) {
-      
+
       # cat("loop entered \n")
       numEmp <- numStaff
     }
-    
+
 
     #####################################
     #####################################
     # Number of companies is the same as the number of employees in the industry
     #####################################
     #####################################
-    
+
     if(numEmp == numStaff) {
 
       Internalemployer <- currentEmp %>%
@@ -119,9 +120,9 @@ createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stf
               pull(ValueIs)
 
             # cat("The last company number is", EndPaste0Value, "\n")
-            
+
             Paste0Value <- EndPaste0Value + 1
-            
+
                 # add the employers to the final dataframe
 
                 if(exists("OutputDataframe")) {
@@ -141,10 +142,10 @@ createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stf
             #####################################
             #####################################
     } else {
-      
+
 
       # note this stackoverflow answer https://stackoverflow.com/a/52559775/1030648
-      
+
           AchievedCompSize <- as.integer(rmultinom(n = 1, size = numStaff, prob = rep(1/numEmp, numEmp)))
 
           # ensure that all companies have at least one employee
@@ -170,32 +171,32 @@ createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stf
             slice_tail(n = 1) %>%
             mutate(ValueIs =  as.numeric(gsub("[^[:digit:].]", "",  CompanyName))) %>%
             pull(ValueIs)
-          
+
           Paste0Value <- EndPaste0Value + 1
-          
+
           if(exists("OutputDataframe")) {
-            
+
             OutputDataframe <- bind_rows(OutputDataframe, Internalemployer)
-            
+
           } else {
-            
+
             OutputDataframe <- Internalemployer
-            
+
             # closes else for if(exists("OutputDataframe"))
           }
-      
+
           #####################################
           #####################################
           # Number of employees > number of employees in the industry block closed
           #####################################
           #####################################
           }
-    
-    
+
+
     # closes for (i in 1:nrow(employerRenamed))
   }
 
- 
+
   # rename the variables
 
   OutputDataframe <- OutputDataframe %>%
@@ -205,9 +206,9 @@ createemp <- function(employers, industry, indsmin, indsmax, pplmin, pplmax, stf
          !!pplmaxcolName := staffCtMax,
          {{stffname}} := Staff,
          {{cpyname}} := CompanyName)
-  
+
 
 
   return(OutputDataframe)
-  
+
 }
