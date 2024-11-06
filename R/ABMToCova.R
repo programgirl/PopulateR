11f15c6341a9fc838e1075a14c403e04c370abc0
programgirl <michelle.a.gosse@gmail.com>
2024-11-06 15:35:25 +1300

    Updated preamble to include instructions for creating the schools layer correctly. Added a test to ensure that at least one school type will be output - Covasim needs a school layer. Covasim will deal with the situation where schools are all shut down.

--- R/ABMToCova.R
+++ R/ABMToCova.R
@@ -1,7 +1,9 @@
 #' Creates a data frame of weighted contact pairs.
 #'
-#' This function creates the household, school, workplace, and contacts layers, from ABMPop, for use with the Python package Covasim. A 1xn dataframe of ages is also created. The data frame must be converted to a Python n x 3 flat dictionary.
-#' 
+#' This function creates the household, school, workplace, and contacts layers, from ABMPop, for use with the Python package Covasim. A 1xn dataframe of ages is also created.
+#'
+#' There are two restrictions for use. First, the codes for preschool, primary school, and secondary school must be set to "P801000", "P802100", and "P802200", respectively. Second, at least one school type must be "Y" as Covasim requires a school layer.
+#'
 #'
 #' @export
 #' @param ABMPop The agent-based modelling dataframe.
@@ -13,117 +15,117 @@
 #' @param PSchool Are primary schools open? Default is "Y", change to "N" if primary schools are to close.
 #' @param SSchool Are secondary schools open? Default is "Y", change to "N" if secondary schools are to close.
 #' @param contacts A data frame consisting of existing contact pairs. The first two variables define the two people in the pair.
-#' @param excludeDF A data frame of industries to exclude. This must be the relevant IndNum variable in the ABMPop data frame. If this data frame is not included, all industries will be represented in the output data frame. 
+#' @param excludeDF A data frame of industries to exclude. This must be the relevant IndNum variable in the ABMPop data frame. If this data frame is not included, all industries will be represented in the output data frame.
 
 #' @return A data frame of the household, school, workplace, contact layers, and people's ages, for use in Covasim.
-#' 
-#' 
-ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool = "Y", SSchool = "Y", contacts = NULL, 
+#'
+#'
+ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool = "Y", SSchool = "Y", contacts = NULL,
                       excludeDF = NULL) {
-  
+
   IDcolName <- sym(names(ABMPop[ABMID]))
   AgeColName <- sym(names(ABMPop[ABMAge]))
   HouseholdIDcolName <- sym(names(ABMPop[place1]))
   PlaceTwocolName <- sym(names(ABMPop[place2]))
 
   ABMPop <- ABMPop %>%
-    rename(ID = !! IDcolName, 
+    rename(ID = !! IDcolName,
            Age = !! AgeColName,
-           HouseholdID = !! HouseholdIDcolName, 
+           HouseholdID = !! HouseholdIDcolName,
            PlaceTwo = !! PlaceTwocolName) %>%
     select(ID, HouseholdID, PlaceTwo, IndCode, IndName, Age)
-  
-  
+
+
   # TODO implement beta other than 1
   # TODO when beta is implemented, must ensure it is between 0 and 1
   # if(is.null(betaDF)) {
-  #   
-  #   
+  #
+  #
   # } else {
-  #   
+  #
   #   Internal <- as.data.frame(betaDF %>%
-  #                               rename(IntAge = !! betaAge, 
+  #                               rename(IntAge = !! betaAge,
   #                                      IntBeta = !! beta2Use))
-  #   
+  #
   #   # cat("Internal constructed \n")
   #   # add sort
-  #   
+  #
   #   if(priority == "Y") {
-  #     
+  #
   #     Internal <- Internal %>%
   #       arrange(desc(IntAge))
-  #     
+  #
   #   }
-  #   
+  #
   #   if(priority == "O") {
-  #     
-  #     
+  #
+  #
   #     Internal <- Internal %>%
   #       arrange(IntAge)
   #   }
-  #   
+  #
   #   # create copy of the output data frame to iterate through
-  #   
+  #
   #   ODFCopy <- OutputDataFrame
-  #   
+  #
   #   cat("ODFCopy made with ", nrow(ODFCopy), "rows \n")
-  #   
+  #
   #   # start applying the replacement betas
-  #   
+  #
   #   for (j in 1:nrow(Internal)) {
-  #     
+  #
   #     # print(j)
-  #     
+  #
   #     CurrentAge <- as.numeric(Internal$IntAge[j])
   #     CurrentBeta <- as.numeric(Internal$IntBeta[j])
-  #     
+  #
   #     # only need their IDs
-  #     
+  #
   #     PeepsThatAge <- ABMPop %>%
   #       filter(Age == CurrentAge) %>%
   #       select(ID)
-  #     
-  #     # cat("CurrentAge is ", CurrentAge, "and CurrentBeta is ", CurrentBeta, "\n") 
-  #     
+  #
+  #     # cat("CurrentAge is ", CurrentAge, "and CurrentBeta is ", CurrentBeta, "\n")
+  #
   #     # locate the people in the Covasim dataset
-  #     
+  #
   #     WorkingPeople <- ODFCopy %>%
   #       filter(p1 %in% PeepsThatAge$ID | p2 %in% PeepsThatAge) %>%
   #       mutate(beta = CurrentBeta)
-  #     
+  #
   #     if(exists("UpdatedBetas")) {
-  #       
+  #
   #       UpdatedBetas <- bind_rows(UpdatedBetas, WorkingPeople)
-  #       
-  #       ODFCopy <- anti_join(ODFCopy, WorkingPeople, by = c("p1", "p2")) 
-  #       
+  #
+  #       ODFCopy <- anti_join(ODFCopy, WorkingPeople, by = c("p1", "p2"))
+  #
   #       # closes if(exists("UpdatedBetas")) {
-  #       
+  #
   #     } else {
-  #       
+  #
   #       UpdatedBetas <- WorkingPeople
-  #       
-  #       ODFCopy <- anti_join(ODFCopy, WorkingPeople, by = c("p1", "p2")) 
-  #       
-  #       
-  #       
+  #
+  #       ODFCopy <- anti_join(ODFCopy, WorkingPeople, by = c("p1", "p2"))
+  #
+  #
+  #
   #       # closes else to if(exists("UpdatedBetas")) {
   #     }
-  #     
+  #
   #     # closes for (j in 1:nrow(Internal)) {
   #   }
-  #   
+  #
   #   OutputDataFrame <- bind_rows(UpdatedBetas, ODFCopy)
-  #   
+  #
   # }
-  
-  
-  
-  
+
+
+
+
   ############################################################################################
   # Household Layer
   ############################################################################################
-  
+
   h1 <- full_join(ABMPop, ABMPop, by = "HouseholdID", relationship = "many-to-many") %>%
     rename(p1 = ID.x,
            p2 = ID.y) %>%
@@ -133,38 +135,43 @@ ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool 
     slice(1) %>%
     ungroup() %>%
     select(-grp)
-  
-# 
+
+#
 #   h0 <- ABMPop %>%
 #     select(ID) %>%
 #     filter(!ID %in% c(h1$p1, h1$p2)) %>%
 #     rename(p1 = ID) %>%
 #     mutate(p2=0)
-  # 
-  # 
+  #
+  #
   # h <- bind_rows(h1, h0) %>%
   #   mutate(beta = 1)
 
-  
+
   h <- h1 %>%
     mutate(beta=1)
-  
-  
-  
-  
+
+
+
+
   ############################################################################################
-  # School Layer 
+  # School Layer
   ############################################################################################
-  
+
   SchoolLayer <- ABMPop %>%
     filter(IndCode %in% c("P801000", "P802100", "P802200"))
 
+  if(ECE == "N" & PSchool == "N" & SSchool == "N") {
+
+    stop("All three school types are set to N. A school layer must be output. \n")
+  }
+
   if(ECE == "N" | PSchool == "N" | SSchool == "N") {
-    
-    cat("At least one educational type is being excluded \n")
-    
-    
-    
+
+    cat("At least one educational type is being excluded. \n")
+
+
+
     # remove ECE
     if(ECE == "N") {
       SchoolLayer <- SchoolLayer %>%
@@ -182,13 +189,13 @@ ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool 
       SchoolLayer <- SchoolLayer %>%
         filter(!IndCode == "P802200")
       }
-    
+
   }
 
   if(nrow(SchoolLayer) > 0) {
-    
+
     Layer2 <- SchoolLayer
-    
+
     s1 <- full_join(SchoolLayer, Layer2, by = "PlaceTwo", relationship = "many-to-many") %>%
       filter(ID.x != ID.y) %>%
       rename(p1 = ID.x,
@@ -200,43 +207,43 @@ ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool 
       select(-grp)
 
   }
-  
+
   # create all the 0s
-  
+
   if(exists("s1")) {
-    
+
     s2 <- ABMPop %>%
       select(ID) %>%
       filter(!ID %in% s1$p1) %>%
       rename(p1=ID) %>%
       mutate(p2=0)
-    
+
    s <- bind_rows(s1,s2) %>%
      mutate(beta = 1)
 
   } else {
-    
+
     s <- ABMPop %>%
       select(ID) %>%
       rename(p1=ID) %>%
       mutate(p2=0) %>%
       distinct() %>%
       mutate(beta = 1)
-    
+
   }
-  
-  
+
+
   ############################################################################################
-  # Workplace Layer 
+  # Workplace Layer
   ############################################################################################
-  
+
   if(is.null(excludeDF)) {
-    
+
     # if there are no industries to exclude, make all the contacts
-  
+
   w1 <- ABMPop %>%
     filter(!IndCode %in% c("P801000", "P802100", "P802200", "Not employed"))
-  
+
   w2 <- full_join(w1, w1, by = "PlaceTwo", relationship = "many-to-many") %>%
     filter(ID.x != ID.y) %>%
     rename(p1 = ID.x,
@@ -246,28 +253,28 @@ ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool 
     slice(1) %>%
     ungroup() %>%
     select(-grp)
-  
+
   # w3 <- ABMPop %>%
   #   select(ID) %>%
   #   filter(!ID %in% w2$p1) %>%
   #   rename(p1 = ID) %>%
   #   mutate(p2 = 0)
-  # 
+  #
   # w <- bind_rows(w2, w3) %>%
   #   mutate(beta = 1)
-  
+
   w <- w2 %>%
     mutate(beta=1)
-  
+
 
   } else {
-    
+
     # there are industries to exclude, remove them from the population
-    
+
     w1 <- ABMPop %>%
       filter(!IndCode %in% c("P801000", "P802100", "P802200", "Not employed")) %>%
       filter(!IndCode %in% excludeDF[,1])
-    
+
     w2 <- full_join(w1, w1, by = "PlaceTwo", relationship = "many-to-many") %>%
       filter(ID.x != ID.y) %>%
       rename(p1 = ID.x,
@@ -277,61 +284,61 @@ ABMToCova <- function(ABMPop, ABMID, ABMAge, place1, place2, ECE = "Y", PSchool 
       slice(1) %>%
       ungroup() %>%
       select(-grp)
-    # 
+    #
     # w3 <- ABMPop %>%
     #   select(ID) %>%
     #   filter(!ID %in% w2$p1) %>%
     #   rename(p1 = ID) %>%
     #   mutate(p2 = 0)
-    # 
+    #
     # w <- bind_rows(w2, w3) %>%
     #   mutate(beta = 1)
-    
+
     w <- w2 %>%
       mutate(beta = 1)
 
   }
-  
- 
+
+
 
   ############################################################################################
-  # Contacts Layer 
+  # Contacts Layer
   ############################################################################################
-  
+
   c1 <- contacts %>%
     rename(p1 = from,
            p2 = to)
-  # 
+  #
   # c2 <- contacts %>%
   #   rename(p1 = to,
   #          p2 = from)
-  # 
+  #
   # c3 <- ABMPop %>%
   #   select(ID) %>%
   #   filter(!ID %in% c(c1$p1, c1$p2)) %>%
   #   rename(p1 = ID) %>%
   #   mutate(p2 = 0)
-# 
+#
 #   c <- bind_rows(c1, c2, c3) %>%
 #     mutate(beta = 1)
-#   
-  
+#
+
   c <- c1 %>%
     mutate(beta=1)
-  
+
   ############################################################################################
-  # Age per person 
+  # Age per person
   # needs to be sorted by ID
   ############################################################################################
-  
+
   age <- ABMPop %>%
     arrange(ID) %>%
     select(Age)
-  
 
 
-  
-  
+
+
+
   OutputDataFrame <- list()
   OutputDataFrame$h <- h
   OutputDataFrame$s <- s
