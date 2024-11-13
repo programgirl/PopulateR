ff4e71c72a8261da3eaf9a6f2a8a779996ba5359
programgirl <michelle.a.gosse@gmail.com>
2024-11-13 17:14:03 +1300

    example linewidth too wide.

--- R/addschool.R
+++ R/addschool.R
@@ -32,9 +32,10 @@ NULL
 #' @examples
 #' library(dplyr)
 #' # children in the same household will be added to the same school, if possible with a .8 probability
-#' SchoolsAdded <- addschool(IntoSchools, pplid = "ID", pplage = "Age", pplsx = "SexCode", pplst = "SchoolStatus",
-#'                           hhid = "HouseholdID", SchoolsToUse, schid = "School.Name", schage = "AgeInRoll",
-#'                           schroll = "RollCount", schtype = "Gender", schmiss = 0, sameprob = .8, userseed = 4)
+#' SchoolsAdded <- addschool(IntoSchools, pplid = "ID", pplage = "Age", pplsx = "SexCode",
+#'                           pplst = "SchoolStatus", hhid = "HouseholdID", SchoolsToUse,
+#'                           schid = "School.Name", schage = "AgeInRoll", schroll = "RollCount",
+#'                           schtype = "Gender", schmiss = 0, sameprob = .8, userseed = 4)
 #'
 #' Population <- SchoolsAdded$Population
 #' Schools <- SchoolsAdded$Schools
