476d09a7cfc9ec9f10a6ebf126b430c9bee39898
programgirl <michelle.a.gosse@gmail.com>
2024-11-14 00:17:11 +1300

    linewidth exceeded 100 characters

--- R/fastmatch.R
+++ R/fastmatch.R
@@ -35,7 +35,8 @@ NULL
 #' # prop is
 #' nrow(NumUnweighted)/nrow(Unweighted)
 #'
-#' # weighted example, same probability, 66% of people in a same-sex relationship are aged between 25 and 54
+#' # weighted example, same probability, 66% of people in a same-sex relationship are aged between 25
+#' and 54
 #' Weighted <- fastmatch(PersonDataframe, pplage = "PersonAge", probSS = 0.03, uwProp = .66,
 #'                       uwLA = 25, uwUA = 54, HHStartNum = 1, HHNumVar = "Household", userseed = 4)
 #' NumWeighted <- Weighted %>%
