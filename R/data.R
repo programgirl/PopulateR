fa4175eb26596462cf68f5caed8b145174cab1f2
programgirl <michelle.a.gosse@gmail.com>
2024-11-16 17:05:30 +1300

    InitialDataframe, IntoSchools, and SingleAges dataframe descriptions finished.

--- R/data.R
+++ R/data.R
@@ -79,40 +79,41 @@
 #' #' }
 #' #'
 #' "GroupInfo"
+
+#' People in age groups, in the Timaru District
 #'
-#' #' People in age groups, in the Timaru District
-#' #'
-#' #' @description
-#' #' Synthetic people produced from combining aggregate Timaru District 2018 census data, using tablecodes 8277 and 8395, sourced from Statistics New Zealand. Statistics New Zealand data are licensed by Stats NZ for reuse under the Creative Commons Attribution 4.0 International licence.
-#' #'
-#' #' @format A data frame with 46,293 rows and 6 variables
-#' #' \describe{
-#' #'     \item{Sex}{Sex of the person}
-#' #'     \item{Age.group}{Age group in five-year age bands}
-#' #'     \item{Relationship}{Relationship status of the person}
-#' #'     \item{LowerAge}{The youngest age in the Age.group}
-#' #'     \item{UpperAge}{The oldest age in the Age.group}
-#' #'     \item{ID}{The unique identifier for the person}
-#' #' }
-#' "InitialDataframe"
+#' @description
+#' A data frame of 46,293 synthetic people. Age groups are present, but not ages.
 #'
-#' #' Four person households, with a school status for each person
-#' #'
-#' #' @description
-#' #' Four-person households, consisting of one parent and three children, with a combination of people in school and not in school. Ages 15 through 18 contain a mixture of people in school and those who have left school. This has been constructed from the Township data frame.
-#' #'
-#' #' @format A data frame of 3,052 rows and 8 variables
-#' #' \describe{
-#' #'     \item{Sex}{Sex of the person}
-#' #'     \item{Relationship}{Relationship status of the person}
-#' #'     \item{ID}{The unique identifier for the person}
-#' #'     \item{Age}{The age of the person}
-#' #'     \item{HoursWorked}{The number of hours worked in employment, per week}
-#' #'     \item{SchoolStatus}{The indicator of whether the person is in school (Y) or not (N)}
-#' #'     \item{HouseholdID}{The household identifier for the person}
-#' #'     \item{SexCode}{Sex, (F)emale or (M)ale}
-#' #' }
-#' "IntoSchools"
+#' @format A data frame with 46,293 rows and 6 variables
+#' \describe{
+#'     \item{Sex}{Sex of the person}
+#'     \item{Age.group}{Age group in five-year age bands}
+#'     \item{Relationship}{Relationship status of the person}
+#'     \item{LowerAge}{The youngest age in the Age.group}
+#'     \item{UpperAge}{The oldest age in the Age.group}
+#'     \item{ID}{The unique identifier for the person}
+#' }
+#' @source Timaru District 2018 census data (tablecodes 8277 and 8395), sourced from Statistics New Zealand. Statistics New Zealand data are licensed by Stats NZ for reuse under the Creative Commons Attribution 4.0 International licence.
+"InitialDataframe"
+
+#' Four person households, with a school status for each person
+#'
+#' @description
+#' Four-person households, consisting of one parent and three children, with a combination of people in school and not in school. Ages 15 through 18 contain a mixture of people in school and those who have left school. This has been constructed from the Township data frame.
+#'
+#' @format A data frame of 3,052 rows and 8 variables
+#' \describe{
+#'     \item{Sex}{Sex of the person}
+#'     \item{Relationship}{Relationship status of the person}
+#'     \item{ID}{The unique identifier for the person}
+#'     \item{Age}{The age of the person}
+#'     \item{HoursWorked}{The number of hours worked in employment, per week}
+#'     \item{SchoolStatus}{The indicator of whether the person is in school (Y) or not (N)}
+#'     \item{HouseholdID}{The household identifier for the person}
+#'     \item{SexCode}{Sex, (F)emale or (M)ale}
+#' }
+"IntoSchools"
 
 #' School leavers
 #'
@@ -168,34 +169,36 @@
 #' @source Canterbury region 2018 census data (tablecode 8277), sourced from Statistics New Zealand. Statistics New Zealand data are licensed by Stats NZ for reuse under the Creative Commons Attribution 4.0 International licence.
 "RegionalStructure"
 
-#' #' Schools and their roll counts
-#' #'
-#' #' @description
-#' #' Nineteen schools in the Canterbury region, with their 2018 roll counts, sourced from the Ministry of Education. The Ministry of Education's data are licensed by the Ministry of Education for reuse under the Creative Commons Attribution 4.0 International licence.
-#' #'
-#' #' @format A data frame with 266 rows and 5 variables
-#' #' \describe{
-#' #'    \item{School.ID}{The numeric ID for the school}
-#' #'    \item{School.Name}{The name for the school}
-#' #'    \item{Gender}{Indicator of whether the school is (C)o-ed, (F)emale-only, or (M)ale-only}
-#' #'    \item{AgeInRoll}{The age of possible students}
-#' #'    \item{RollCount}{The number of students. The value is 0 if no students that age attend.}
-#' #' }
-#' "SchoolsToUse"
+#' Schools and their roll counts
 #'
-#' #' Sex/Age pyramid data for Timaru District
-#' #'
-#' #' @description
-#' #' The number of people, by age and sex, living in the Timaru District. The data is based on 2018 census data in tablecode 8277, sourced from Statistics New Zealand. Statistics New Zealand data are licensed by Stats NZ for reuse under the Creative Commons Attribution 4.0 International licence.
-#' #'
-#' #' @format A data frame with 190 rows and 4 variables
-#' #' \describe{
-#' #'     \item{Age.group}{Age group, in five-year age bands}
-#' #'     \item{Sex}{Sex, either Male or Female}
-#' #'     \item{Value}{The number of people that age and sex}
-#' #'     \item{Age}{Age at last birthday}
-#' #' }
-#' "SingleAges"
+#' @description
+#' Nineteen schools in the Canterbury region, with their 2018 roll counts.
+#'
+#' @format A data frame with 266 rows and 5 variables
+#' \describe{
+#'    \item{School.ID}{The numeric ID for the school}
+#'    \item{School.Name}{The name for the school}
+#'    \item{Gender}{Indicator of whether the school is (C)o-ed, (F)emale-only, or (M)ale-only}
+#'    \item{AgeInRoll}{The age of possible students}
+#'    \item{RollCount}{The number of students. The value is 0 if no students that age attend.}
+#' }
+#' @source The Ministry of Education. The Ministry of Education's data are licensed by the Ministry of Education for reuse under the Creative Commons Attribution 4.0 International licence.
+"SchoolsToUse"
+
+#' Sex/Age pyramid data for Timaru District
+#'
+#' @description
+#' The number of people, by age and sex, living in the Timaru District.
+#'
+#' @format A data frame with 190 rows and 4 variables
+#' \describe{
+#'     \item{Age.group}{Age group, in five-year age bands}
+#'     \item{Sex}{Sex, either Male or Female}
+#'     \item{Value}{The number of people that age and sex}
+#'     \item{Age}{Age at last birthday}
+#' }
+#' @source Timaru District 2018 census data (tablecodes 8277), sourced from Statistics New Zealand. Statistics New Zealand data are licensed by Stats NZ for reuse under the Creative Commons Attribution 4.0 International licence.
+"SingleAges"
 
 #' Simulated township
 #'
