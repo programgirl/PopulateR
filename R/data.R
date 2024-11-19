d79aa5f005f85d40f0ef9218a9ae3fa618b01f6f
programgirl <michelle.a.gosse@gmail.com>
2024-11-20 09:55:44 +1300

    All @format lines end with : Sex description changed to categories contained. WithSchoolInd deleted as no longer required.

--- R/data.R
+++ R/data.R
@@ -3,9 +3,9 @@
 #' @description
 #' A subset of people from the Township data frame, aged 20 years and older with a relationship status of "NonPartnered".
 #'
-#' @format A data frame of 2,213 rows and 5 variables
+#' @format A data frame of 2,213 rows and 5 variables:
 #' \describe{
-#'     \item{Sex}{Sex of the person}
+#'     \item{Sex}{SEither Male or Female}
 #'     \item{Relationship}{Relationship status of the person}
 #'     \item{ID}{The unique identifier for the person}
 #'     \item{Age}{The age of the person}
@@ -18,7 +18,7 @@
 #' @description
 #' The number of businesses and employees by industry, Timaru District, for 2018.
 #'
-#' @format A data frame of 183 rows and 7 variables
+#' @format A data frame of 183 rows and 7 variables:
 #' \describe{
 #'     \item{ANZSIC06}{The code and associated name for each industry}
 #'     \item{BusinessCount}{The random-rounded count of employers in the industry}
@@ -36,9 +36,9 @@
 #' @description
 #' A subset of people from the Township data frame, aged between 20 and 91 years. Age bands, and the associated minimum and maximum ages, have been added.
 #'
-#' @format A data frame of 7,568 rows and 8 variables
+#' @format A data frame of 7,568 rows and 8 variables:
 #' \describe{
-#'     \item{Sex}{Sex of the person}
+#'     \item{Sex}{Either Male or Female}
 #'     \item{Relationship}{Relationship status of the person}
 #'     \item{ID}{The unique identifier for the person}
 #'     \item{Age}{The age of the person}
@@ -54,7 +54,7 @@
 #' @description
 #' Synthetic employers and their associated number of employees, randomly constructed using the "AllEmployers" data frame.
 #'
-#' @format A data frame of 225 rows and 3 variables
+#' @format A data frame of 225 rows and 3 variables:
 #' \describe{
 #'     \item{ANZSIC06}{The code and associated name for the industry associated with the employer}
 #'     \item{NumEmployees}{The count of employees for the employer}
@@ -68,7 +68,7 @@
 #' @description
 #' The estimated proportion of people in a relationship, by age band within sex, for people aged between 20 and 90 years.
 #'
-#' @format A data frame of 14 rows and 7 variables
+#' @format A data frame of 14 rows and 7 variables:
 #' \describe{
 #'     \item{Sex}{Either Male or Female}
 #'     \item{AgeBand}{The 10-year age band}
@@ -86,9 +86,9 @@
 #' @description
 #' A data frame of 46,293 synthetic people. Age groups are present, but not ages.
 #'
-#' @format A data frame with 46,293 rows and 6 variables
+#' @format A data frame with 46,293 rows and 6 variables:
 #' \describe{
-#'     \item{Sex}{Sex of the person}
+#'     \item{Sex}{Either Male or Female}
 #'     \item{Age.group}{Age group in five-year age bands}
 #'     \item{Relationship}{Relationship status of the person}
 #'     \item{LowerAge}{The youngest age in the Age.group}
@@ -103,16 +103,16 @@
 #' @description
 #' Four-person households, consisting of one parent and three children, with a combination of people in school and not in school. Ages 15 through 18 contain a mixture of people in school and those who have left school. This has been constructed from the Township data frame.
 #'
-#' @format A data frame of 980 rows and 8 variables
+#' @format A data frame of 980 rows and 8 variables:
 #' \describe{
-#'     \item{Sex}{Sex of the person}
+#'     \item{Sex}{Either Male or Female}
 #'     \item{Relationship}{Relationship status of the person}
 #'     \item{ID}{The unique identifier for the person}
 #'     \item{Age}{The age of the person}
 #'     \item{HoursWorked}{The number of hours worked in employment, per week}
 #'     \item{SchoolStatus}{The indicator of whether the person is in school (Y) or not (N)}
 #'     \item{HouseholdID}{The household identifier for the person}
-#'     \item{SexCode}{Sex, (F)emale or (M)ale}
+#'     \item{SexCode}{Either (F)emale or (M)ale}
 #' }
 "IntoSchools"
 
@@ -121,7 +121,7 @@
 #' @description
 #' School leavers in the Canterbury Region, counts by age and sex, for the period 2009 to 2018.
 #'
-#' @format A data frame with 120 rows and 4 variables
+#' @format A data frame with 120 rows and 4 variables:
 #' \describe{
 #'     \item{YearLeft}{The year for the school leaver count}
 #'     \item{Sex}{The sex for the school leaver count}
@@ -142,11 +142,11 @@
 #' Synthetic people living in the Timaru District
 #'
 #' @description
-#' 1000 synthetic people.
+#' 1000 synthetic people, to match the number of people in the NetworkMatrix data frame.
 #'
-#' @format A data frame with 1,000 rows and 5 variables
+#' @format A data frame with 1,000 rows and 5 variables:
 #' \describe{
-#'     \item{Sex}{Sex of the person}
+#'     \item{Sex}{Either Male or Female}
 #'     \item{Relationship}{Relationship status of the person}
 #'     \item{ID}{The unique identifier for the person}
 #'     \item{Age}{The age of the person}
@@ -160,7 +160,7 @@
 #' @description
 #' The number of people, by age and sex, living in the Canterbury region, restricted to ages 13 to 19 years.
 #'
-#' @format A data frame with 14 observations and 4 variables
+#' @format A data frame with 14 observations and 4 variables:
 #' \describe{
 #'     \item{Sex}{The sex relating to the count}
 #'     \item{Age.group}{String variable of age plus the text " years"}
@@ -175,7 +175,7 @@
 #' @description
 #' Nineteen schools in the Canterbury region, with their 2018 roll counts.
 #'
-#' @format A data frame with 266 rows and 5 variables
+#' @format A data frame with 266 rows and 5 variables:
 #' \describe{
 #'    \item{School.ID}{The numeric ID for the school}
 #'    \item{School.Name}{The name for the school}
@@ -191,10 +191,10 @@
 #' @description
 #' The number of people, by age and sex, living in the Timaru District.
 #'
-#' @format A data frame with 190 rows and 4 variables
+#' @format A data frame with 190 rows and 4 variables:
 #' \describe{
 #'     \item{Age.group}{Age group, in five-year age bands}
-#'     \item{Sex}{Sex, either Male or Female}
+#'     \item{Sex}{Either Male or Female}
 #'     \item{Value}{The number of people that age and sex}
 #'     \item{Age}{Age at last birthday}
 #' }
@@ -217,30 +217,14 @@
 #' @source Timaru District 2018 census data, using tablecodes 8277, 8395, and 8460, sourced from Statistics New Zealand. Statistics New Zealand data are licensed by Stats NZ for reuse under the Creative Commons Attribution 4.0 International licence.
 "Township"
 
-#' #' People with a school indicator
-#' #'
-#' #' @description
-#' #' A modified version of "Township" data frame with a school indicator variable added.
-#' #'
-#' #' @format A data frame with 10,000 rows and 6 variables
-#' #' \describe{
-#' #'     \item{Sex}{Sex of the person}
-#' #'     \item{Relationship}{Relationship status of the person}
-#' #'     \item{ID}{The unique identifier for the person}
-#' #'     \item{Age}{The age of the person}
-#' #'     \item{HoursWorked}{The number of hours worked in employment, per week. This is an ordered factor.}
-#' #'     \item{SchoolStatus}{The indicator of whether the person is in school (Y) or not (N)}
-#' #' }
-#' "WithSchoolInd"
-
 #' Adolescents with a school status and employment hours
 #'
 #' @description
 #' A set of synthetic adolescents aged between 15 and 18.
 #'
-#' @format A data frame of 546 observations and 6 variables
+#' @format A data frame of 478 observations and 6 variables:
 #' \describe{
-#'     \item{Sex}{Sex of the person}
+#'     \item{Sex}{Either Male or Female}
 #'     \item{Relationship}{Relationship status of the person}
 #'     \item{ID}{The unique identifier for the person}
 #'     \item{Age}{Age of the person}
